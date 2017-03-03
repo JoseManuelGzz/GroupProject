09615ba0f81bf3e0cff858f23ce860e32110949e
José Manuel González <jmanuelgc94@gmail.com>
2017-03-03 16:26:58 +0000

    Simulation Class

---
+++ Simulation.py
@@ -0,0 +1,10 @@
+
+class Simulation:
+
+	def __init__(self, algorithm, status):
+		self.algorithm = algorithm
+		self.status = status
+
+	def get_solution():
+		algorithm.run_problem(algorithm, status)
+
