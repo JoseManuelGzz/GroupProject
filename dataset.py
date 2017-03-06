055a1afb14bb3ab79a954543a5bc75c10d55330f
jcvillarrealg <joseca.villarreal@gmail.com>
2017-03-06 23:24:03 +0000

    Attributes added to Dataset class
    Attributes to keep track of the set of characters in a column and the
    most recurrent character in a column were added to the Dataset class as
    well as their calculation

--- dataset.py
+++ dataset.py
@@ -1,9 +1,12 @@
 import random
+from collections import Counter
 class Dataset:
     def __init__(self, rows=100, cols=10, alphabet=range(0,10)):
         self.rows = rows
         self.alphabet = alphabet
         self.cols = cols
+        self.alphabet_per_column = []
+        self.most_common_char = []
         
     def get_data(self):
         """
@@ -16,6 +19,21 @@ class Dataset:
         d = []
         for _ in range(self.rows):
             d.append([random.choice(self.alphabet) for _ in range(self.cols)])
+
+        print("Dataset")
+        print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in d]))
+
+        #Building the array with the sets of alphabet in each column
+        #and
+        #Building the array with the most common character in each column
+        for i in range(self.cols):
+            column = zip(*d)[i]
+            print(column)
+            self.alphabet_per_column.append(list(set(column)))
+            print(self.alphabet_per_column[i])
+            self.most_common_char.append(Counter(column).most_common(1)[0][0])
+            print(self.most_common_char[i])
+            print("---")
         return d
 
 
