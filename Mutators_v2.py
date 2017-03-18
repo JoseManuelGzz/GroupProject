ecf5910da5bfbbac2ad1fc980ae21596aad13b48
José Manuel González <jmanuelgc94@gmail.com>
2017-03-18 23:29:27 +0000

    Added comments to the Mutations_v2 class

--- Mutators_v2.py
+++ Mutators_v2.py
@@ -1,29 +1,56 @@
-"""
-Class mutators
-            This class is used as a parent class to two additional classes.
-            It contains an initialiser which takes the dataset as an argument
-            and a second method called mutate used by the sub-classes
-            Sub-class: ExpMutator
-            Sub-class:RandomFlip
-"""
 import random as random
 import string
 
-class mutators:
-    
+"""
+
+<Mutators>: <Class that contains the different mutating operators
+             as subclasses>
+
+"""
+class Mutators:
+    """
+    <__init__>
+            <Constructor of the parent Mutators class>
+            parameters:
+                    <self> - Pointer to the object
+                    <dataset> - A copy of the dataset object
+            returns:
+                    -NA-
+    """
     def __init__(self, dataset):
         self.dataset = dataset
-    
-    def mutate(self, solution):
-        pass
 
+"""
 
-class ExpMutator(mutators):
-    
+<ExpMutator>: <Mutator subclass that implements the Exponential 
+                mutation>
+
+"""
+class ExpMutator(Mutators):
+    """
+    <__init__>
+            <Constructor of the Exponential mutator subclass>
+            parameters:
+                    <self> - Pointer to the object
+                    <dataset> - A copy of the dataset object
+                    <prob> - Probability of mutation
+            returns:
+                    -NA-
+    """
     def __init__(self, dataset, prob):
         self.dataset = dataset
         self.prob = prob
     
+    """
+    <mutate>
+            <Method that implements the exponential mutation on the 
+             current solution to the problem>
+            parameters:
+                    <self> - Pointer to the object
+                    <solution> - A list with the current solution to the problem
+            returns:
+                    <solution> - An updated list with the solution after undergoing mutation
+    """
     def mutate(self, solution):
         i = random.randint(0, len(solution)-1)
         curr = i
@@ -45,11 +72,37 @@ class ExpMutator(mutators):
                 break
         return solution
 
-class RandomFlip(mutators):
+"""
+
+<RandomFlip>: <Mutator subclass that implements the Random Flip 
+                mutation>
+
+"""
+class RandomFlip(Mutators):
+    """
+    <__init__>
+            <Constructor of the Random Flip mutator subclass>
+            parameters:
+                    <self> - Pointer to the object
+                    <dataset> - A copy of the dataset object
+                    <prob> - Probability of mutation
+            returns:
+                    -NA-
+    """
     def __init__(self, dataset, prob):
         self.dataset = dataset
         self.prob = prob
     
+    """
+    <mutate>
+            <Method that implements the random flip mutation on the 
+             current solution to the problem>
+            parameters:
+                    <self> - Pointer to the object
+                    <solution> - A list with the current solution to the problem
+            returns:
+                    <solution> - An updated list with the solution after undergoing mutation
+    """
     def mutate(self, solution):
         i = 0
         alphabet_per_column = self.dataset.get_alphabet_per_column()
