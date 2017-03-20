9bf09af59246a45bdf4cf03d580301d488a078e5
jcvillarrealg <joseca.villarreal@gmail.com>
2017-03-20 15:23:03 +0000

    Added best solution attribute to the status class and completed some documentation

--- Status.py
+++ Status.py
@@ -17,7 +17,7 @@ class Status:
                         sequence_length - number of characters in each sequence
                         alphabet_size - number of possible characters in the sequences
                 returns:
-                        -None-
+                        -NA-
 
         """
         def __init__(self, max_iter, num_sequences, sequence_length, alphabet_size):
@@ -34,134 +34,149 @@ class Status:
                 self.mcm_parameters = {}
                 self.re_parameters = {}
                 self.solution_record = []
+                self.best_solution = []
 
         """
         add_iteration
                 Add one iteration to the object's counter for iterations
                 parameters:
-                        <param-1> - 
+                        -NA- 
                 returns:
-                        <description>
+                        -NA-
 
         """
         def add_iteration(self):
                 self.num_iter = self.num_iter + 1
 
         """
-        <func_name>
-                <description>
+        add_function_calls
+                Add one function call to the object's counter
                 parameters:
-                        <param-1> - 
+                        -NA-
                 returns:
-                        <description>
+                        -NA-
 
         """
         def add_function_calls(self):
                 self.func_calls = self.func_calls + 1
 
         """
-        <func_name>
-                <description>
+        add_function_evaluation
+                Add a new evaluation of the functionto the object's counter, 
+                        which represents the quality of the current proposed solution 
+                        at a given iteration
                 parameters:
-                        <param-1> - 
+                        func_value - number representing the objective function
+                                evaluation
                 returns:
-                        <description>
-
+                        -NA-
+                        
         """
         def add_function_evaluation(self, func_value):
                 self.func_evaluation.append(func_value)
 
         """
-        <func_name>
-                <description>
+        add_c_sa_parameters
+                Keep track of the temperature parameter, useful for the 
+                        Simulated Annealing Algorithm simulation
                 parameters:
-                        <param-1> - 
+                        c_value - temperature value given by the user to
+                                keep track
                 returns:
-                        <description>
+                        -NA-
 
         """
         def add_c_sa_parameters(self, c_value):
                 self.sa_parameters["c"].append(c_value)
 
         """
-        <func_name>
-                <description>
+        add_solution_record_entry
+                Add an entry to the collection of lists that represent
+                        the behaviour of a simulation across its multiple iterations
                 parameters:
-                        <param-1> - 
+                        entry - a list with standard values representing the current
+                                iteration, the number of function calls, the best evaluation
+                                so far and the current evaluation
                 returns:
-                        <description>
+                        -NA-
 
         """
         def add_solution_record_entry(self, entry):
                 self.solution_record.append(entry)
 
         """
-        <func_name>
-                <description>
+        set_alpha_value_sa
+                Update the alpha parameter for the Simulated Annealing simulations
                 parameters:
-                        <param-1> - 
+                        alpha - numeric value representing the alpha to be used 
                 returns:
-                        <description>
+                        -NA-
 
         """
         def set_alpha_value_sa(self, alpha):
                 self.sa_parameters['alpha'] = alpha
 
         """
-        <func_name>
-                <description>
+        set_elapsed_time
+                Function that enables the user to keep track of the time
+                        that an algorithm takes to run completely
                 parameters:
-                        <param-1> - 
+                        elapsed_time - numeric value representing the number of
+                                elapsed from start to finish 
                 returns:
-                        <description>
+                        -NA-
 
         """
         def set_elapsed_time(self, elapsed_time):
                 self.elapsed_time = elapsed_time
 
         """
-        <func_name>
-                <description>
+        get_function_calls
+                retrieve the number of cuntion calls  in the current status object
                 parameters:
-                        <param-1> - 
+                        -NA- 
                 returns:
-                        <description>
+                        The number that represents how many times the objective
+                                function was called
 
         """
         def get_function_calls(self):
                 return self.func_calls
 
         """
-        <func_name>
-                <description>
+        get_max_iterations
+                Function to retrieve the store number of max iterations
                 parameters:
-                        <param-1> - 
+                        -NA-
                 returns:
-                        <description>
+                        Numeric value representing the maximum iterations set 
+                        on the object
 
         """
         def get_max_iterations(self):
                 return self.max_iter
 
         """
-        <func_name>
-                <description>
+        get_solution_record
+                Retrieve the attribute of the object thar stores the information
+                        about the simulation run
                 parameters:
-                        <param-1> - 
+                        -NA- 
                 returns:
-                        <description>
+                        A list of lists with the information about an algorithm run
 
         """
         def get_solution_record(self):
                 return self.solution_record
 
         """
-        <func_name>
-                <description>
+        save_to_file
+                Function that enables the user to save the solution_record entries
+                        to a csv file
                 parameters:
-                        <param-1> - 
+                        file_name - String with the path for the file to be created
                 returns:
-                        <description>
+                        -NA-
 
         """
         def save_to_file(self, file_name):
@@ -170,13 +185,13 @@ class Status:
                         csv_obj.writerows(self.solution_record)
 
         """
-        <func_name>
-                <description>
+        load_from_file
+                Function that enables a user to load a solution_record from a 
+                CSV file
                 parameters:
-                        <param-1> - 
+                        file_name - String with the path for the file to be loaded 
                 returns:
-                        <description>
-
+                        -NA-
         """
         def load_from_file(self, file_name):
                 result = []
@@ -186,3 +201,27 @@ class Status:
                                 result.append(row)
                 self.solution_record = result
 
+        """
+        set_best_solution
+                Function tha enables the user to save the best list of characters
+                        that represent a solution to an instance of a String Consensus
+                        Algorithm
+                parameters:
+                        solution - list containing a sequence of characters with the best solution 
+                returns:
+                        -NA-
+        """
+        def set_best_solution(self, solution):
+                self.best_solution = solution
+
+        """
+        get_best_solution
+                Function that enables the user to retrieve the best solution stored for 
+                        a given instance of a String Consenus Problem simulation
+                parameters:
+                        -NA-
+                returns:
+                        the list containing the best solution
+        """
+        def get_best_solution(self):
+                return self.best_solution
