541f59529f4dc0c653f5e9bfa019d27fd6c3dd79
ab16889 <ab16889@essex.ac.uk>
2017-03-20 20:55:13 +0000

    Dataset

--- Dataset.py
+++ Dataset.py
@@ -1,6 +1,17 @@
 import random
 from collections import Counter
 class Dataset:
+    """
+            __init__
+                    COnstructor for the Dataset class
+                    parameters:
+                            rows:  size of the dataset (number of sequences)
+                            cols: size of each sequence - columns
+                            alphabet: characters from which the sequences are built
+                    returns:
+                            -None-
+
+        """
     def __init__(self, rows=100, cols=10, alphabet=range(0,10)):
         self.rows = rows
         self.alphabet = alphabet
@@ -11,22 +22,20 @@ class Dataset:
         for _ in range(self.rows):
             self.d.append([random.choice(self.alphabet) for _ in range(self.cols)])
 
-        #print("Dataset")
-        #print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in self.d]))
-
-        #Building the array with the sets of alphabet in each column
-        #and
-        #Building the array with the most common character in each column
         for i in range(self.cols):
             column = zip(*self.d)[i]
-            #print(column)
-            #self.alphabet_per_column.append(list(set(column)))
             self.alphabet_per_column.append(list(column))
-            #print(self.alphabet_per_column[i])
             self.most_common_char.append(Counter(column).most_common(1)[0][0])
-            #print(self.most_common_char[i])
-            #print("---")
-        
+
+    """
+            get_data
+                    Get data from dataset
+                    parameters:
+                            None
+                    returns:
+                            self.d --> (data from the dataset)
+
+        """
     def get_data(self):
         """
         returns a list of lists.
@@ -36,16 +45,52 @@ class Dataset:
         Number of items in each list are determined by the self.cols variable
         """
         return self.d
-    
+
+    """
+            get_most_common_char_column
+                    returns most common character from a column
+                    parameters:
+                            -None-
+                    returns:
+                            self.most_common_char --> (Most common character from the columns, it is a sequence of the length self.cols)
+
+        """
     def get_most_common_char_column(self):
         return self.most_common_char
 
+    """
+                get_alphabet_per_column
+                        returns all characters used in a column
+                        parameters:
+                                -None-
+                        returns:
+                                self.alphabet_per_column --> (All characters from the columns, returned as list of lists)
+
+     """
     def get_alphabet_per_column(self):
         return self.alphabet_per_column
 
+    """
+                    get_cols
+                            returns no of columns in a dataset
+                            parameters:
+                                    -None-
+                            returns:
+                                    self.cols
+
+    """
     def get_cols(self):
         return self.cols
 
+    """
+                        get_alphabet
+                                returns alphabet used in the dataset
+                                parameters:
+                                        -None-
+                                returns:
+                                        self.alphabet
+
+        """
     def get_alphabet(self):
         return self.alphabet
 
